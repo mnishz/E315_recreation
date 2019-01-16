70cdfce3127b0962a18d444d8612f34e99a316f9
mnishz <mnishz@users.noreply.github.com>
2019-01-16 14:21:22 +0000

    initial commit

---
+++ print_9500_lines.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// #include <unistd.h>
+
+int main(void)
+{
+//     for (int i = 0; (i < 10); ++i) {
+        for (int j = 0; (j < 9500); ++j) {
+            printf("%d\n", j);
+        }
+//         sleep(1);
+//     }
+    return 0;
+}
